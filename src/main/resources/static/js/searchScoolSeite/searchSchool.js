9e8a30f7cf3f16cf3e4b5a4a699d668cc20bdb56
kartabas <olenicholeksandr@gmail.com>
2023-12-21 19:57:14 +0100

    WEB: fix bags with (delete noFound element )

--- src/main/resources/static/js/searchScoolSeite/searchSchool.js
+++ src/main/resources/static/js/searchScoolSeite/searchSchool.js
@@ -2,39 +2,39 @@
 let countClick = 0;
 $(document).ready(function () {
 
-	$("#searchButton").click(function () {
+	$("#searchButton").click(function (event) {
 
 		let searchText = $("#searchSchool").val();
 
 		countClick = countClick + 1;
 		$(".search_school").css({
-			display:"block"
+			display: "block"
 		});
 
+
+
 		if (countClick < 2) {
-
-
-			if (searchText == "" ) {
+			if (searchText == "") {
+			    event.preventDefault();
+                $(".json__object").remove();
 				$(".search-wrapper").addClass("error");
-				
-				$(".noFound").append("<span>Your school not found...</span>");
+                $(".noFound").html("<span>Your school not found...</span>");
 				console.log("Your school not found...");
+
+
 			}
-            
-		}else {
-             $(".noFound span").remove();
-				 $(".search-wrapper").removeClass("error");
+		} else {
+
+			$(".noFound span").remove();
+			$(".search-wrapper").removeClass("error");
+			countClick = 0;
+		}
+
+		countClick++;
 
 
-				 $(".search_school").css({
-					display:"none"
-				});
-		
-            countClick = 0;
-        }
 
-
-	});
+});
 
 
 
