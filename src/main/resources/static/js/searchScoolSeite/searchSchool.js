bb9d0bfeca135f108ab0b7285ba08de81413c703
kartabas <olenicholeksandr@gmail.com>
2023-12-21 18:38:40 +0100

    WEB: fixed bag with search input and add error validation

--- src/main/resources/static/js/searchScoolSeite/searchSchool.js
+++ src/main/resources/static/js/searchScoolSeite/searchSchool.js
@@ -7,21 +7,29 @@ $(document).ready(function () {
 		let searchText = $("#searchSchool").val();
 
 		countClick = countClick + 1;
-//		$("#searchResult").css({
-//        	display: "block"
-//        });
+		$(".search_school").css({
+			display:"block"
+		});
 
 		if (countClick < 2) {
 
 
 			if (searchText == "" ) {
-
-				$(".error").append("<span>Your school not found...</span>");
+				$(".search-wrapper").addClass("error");
+				
+				$(".noFound").append("<span>Your school not found...</span>");
+				console.log("Your school not found...");
 			}
-            console.log("Your school not found...");
+            
 		}else {
-             $(".error").remove();
+             $(".noFound span").remove();
+				 $(".search-wrapper").removeClass("error");
 
+
+				 $(".search_school").css({
+					display:"none"
+				});
+		
             countClick = 0;
         }
 
