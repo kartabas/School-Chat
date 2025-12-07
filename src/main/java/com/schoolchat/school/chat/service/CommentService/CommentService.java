0cf348377360c9ab9f00ec29dc014fcc8aaef9c8
kartabas <olenicholeksandr@gmail.com>
2025-12-07 16:26:59 +0100

    BACK:fix bag with CommentService

--- src/main/java/com/schoolchat/school/chat/service/CommentService/CommentService.java
+++ src/main/java/com/schoolchat/school/chat/service/CommentService/CommentService.java
@@ -1,7 +1,6 @@
-package com.schoolchat.school.chat.service.commentService;
+package com.schoolchat.school.chat.service.CommentService;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
