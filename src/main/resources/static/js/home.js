ce19b3803713309e0ee3f753d39757324fb4e146
kartabas <olenicholeksandr@gmail.com>
2025-12-08 18:47:15 +0100

    WEB: fix bag with DefaultLike

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -94,7 +94,7 @@ $(document).ready(function () {
 			$likeBtn.attr("data-liked", "true");
 
 		} else {
-			$likeImg.attr("src", "../../../fotos/profile/defaultLike.png");
+			$likeImg.attr("src", "../../../fotos/profile/DefaultLike.png");
 			$likeCount.text(function (i, text) {
 				return parseInt(text) - 1;
 			});
